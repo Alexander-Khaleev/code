737a30003a9a6a06cf83459baf73535bc55e80c7
Alexander-Khaleev <akhaleev@mail.ru>
2021-04-21 15:00:50 +0300

    Delete simple_shopping_cart_angular2 directory

--- simple_shopping_cart_angular2/app/goods.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Goods = (function () {
-    function Goods() {
-    }
-    return Goods;
-}());
-exports.Goods = Goods;
-//# sourceMappingURL=goods.js.map
--- simple_shopping_cart_angular2/app/goods.ts
+++
@@ -1,6 +0,0 @@
-export class Goods {
-    photo: string;
-	model: string;
-	price: number;
-	count: number;
-}
